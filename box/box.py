aaae94caaf20b271299051a030eef54ccc0c0f5f
ckrintz <ckrintz@gmail.com>
2017-01-15 17:06:49 -0800

    testing

--- box/box.py
+++ box/box.py
@@ -1,7 +1,7 @@
 '''Author: Chandra Krintz, UCSB, ckrintz@cs.ucsb.edu, AppScale BSD license
 
-   USAGE: python box.py BOX_SECRET BOX_CLIENT BOX_APPNAME PREFIX PREFIX_BOXID BOX_FILEID
-   python box.py "..." "..." "..." "Windmill Canyon 1" "5802215677" "95599079123"
+   USAGE: python box.py BOX_SECRET BOX_CLIENT BOX_APPNAME BOX_FILEID
+   python box.py "..." "..." "..." "95599079123"
 
    The program performs the Oauth2 handshake to have the user retrieve the code.
    It stores the access and refresh token in a file using json for future use.
@@ -12,51 +12,39 @@
    from box.com through the app using oauth2 tokens, and stores it in thumbnail.png 
    in the current working directory.
 
-   required: exifread, boxsdk
+   required: boxsdk
 '''
 
-import random,argparse,json,os,sys,time,exifread,cv2
-from datetime import datetime, timedelta
+import argparse,json,os,sys,time
 from contextlib import contextmanager #for timeblock
 from boxsdk import Client, OAuth2
 from boxsdk.exception import BoxOAuthException
 import oauth2_cli_util
 
 DEBUG = False
-######################## timer utility ############################
-@contextmanager
-def timeblock(label): 
-    start = time.time() #time.process_time() available in python 3
-    try:
-        yield
-    finally:
-        end = time.time()
-        print ('{0} : {1:.10f} secs'.format(label, end - start))
 
 ############## main #################
 def main():
     global DEBUG
-    parser = argparse.ArgumentParser(description='Download a file as a thumbnail from box with metadata')
+    parser = argparse.ArgumentParser(description='Download a file as a thumbnail from box')
     parser.add_argument('sec',action='store',help='Box Secret')
     parser.add_argument('cli',action='store',help='Box Client')
     parser.add_argument('app',action='store',help='Box App Name')
 
-    parser.add_argument('prefix',action='store',help='Prefix string')
-    parser.add_argument('prefix_id',action='store',help='Prefix ID in Box')
     parser.add_argument('fid',action='store',help='File ID in Box')
     args = parser.parse_args()
 
-    prefix_list=[(args.prefix,args.prefix_id)]
     client=args.cli
     secret=args.sec
     fid=args.fid
     app = args.app
 
-    #setup OAuth2
+    #acquire OAuth2 tokens and store them if generated, refresh if expired
     #make sure that this is one of the redirect URIs in the oauth setup on box app
     redir_uri='http://localhost'
     oauth2_cli_util.setupStorage(app,client,redir_uri)
-    tok,ref = oauth2_cli_util.getTokens()
+    creds = oauth2_cli_util.getTokens()
+    tok,ref = creds['access_token'],creds['refresh_token']
     if DEBUG:
         print 'Initial Access Token is: {0}'.format(tok)
     if tok is None:
@@ -80,7 +68,7 @@ def main():
     #for testing - this will cause refresh of the token if needed
     #print oauth2_cli_util.query('https://api.box.com/2.0/users/me')
 
-    res = oauth2_cli_util.get_thumbnail(fid)
+    res = oauth2_cli_util.get_thumbnail(fid) #uses requests not auth client
     print 'Return Message: {0}'.format(res)
 
 
--- box/oauth2_cli_util.py
+++ box/oauth2_cli_util.py
@@ -1,49 +1,62 @@
 '''Author: Chandra Krintz, UCSB, ckrintz@cs.ucsb.edu, AppScale BSD license
    library used for oauth2 handshake using Google's Oauth2 lib
    https://developers.google.com/api-client-library/python/start/get_started 
-   required: google-api-python-clien
+   required: google-api-python-client
 
    USAGE: import oauth2_cli_util
 '''
 
-import json, traceback, requests, sys, argparse, csv
+import json, traceback, requests, sys, argparse, uuid, os
 from datetime import datetime, timedelta
-from urllib import urlencode
-import logging
+#from urllib import urlencode
+from contextlib import contextmanager #for timeblock
 #Google APIs OAuth2 library: pip install --upgrade google-api-python-client
-from oauth2client import client as oclient
-from oauth2client.file import Storage as ostorage
+from boxsdk import OAuth2
+from boxsdk import Client
+from boxsdk.network.default_network import DefaultNetwork
+import boxsdk.exception 
+from pprint import pformat
 
 DEBUG = True
-CLI_session = {}
 #this is set in intialize_storage which must be run before all else, app entry is /
-app_name = 'XXX'
-service = 'YYY'
-storage = None
+clientID = 'YYY'
+secret = 'ZZZ'
 redir_uri = None
+token_uri = None
+boxClient = None
 
 #You will also need a box login and password, and have added an app in box
 #to obtain the client/secret above and to set/get the redirect url
 
-################ initialize_storage ##################
-def initialize_storage():
-  '''
-  Utility to grab the credentials from storage if any
-  -- from https://developers.google.com/api-client-library/python/guide/aaa_oauth
-  '''
-  global storage
-  print 'oauth2_cli_util:in initialize_storage {0}, {1}'.format(app_name,service)
-  if storage is None:
-      fname = '{0}_{1}'.format(app_name,service)
-      storage = ostorage(fname)
-  credentials = storage.get()
-  if credentials is not None:
-      CLI_session['credentials'] = oclient.OAuth2Credentials.to_json(credentials)
-      if DEBUG:
-          creds = json.loads(CLI_session['credentials'])
-      return True
-  return False
-    
+########## Logging Network Class ###################
+class LoggingNetwork(DefaultNetwork):
+    #from http://opensource.box.com/box-python-sdk/tutorials/intro.html
+    def request(self, method, url, access_token, **kwargs):
+        """ Base class override. Pretty-prints outgoing requests and incoming responses. """
+        print '\x1b[36m{} {} {}\x1b[0m'.format(method, url, pformat(kwargs))
+        response = super(LoggingNetwork, self).request(
+            method, url, access_token, **kwargs
+        )
+        if response.ok:
+            print '\x1b[32m{}\x1b[0m'.format(response.content)
+        else:
+            print '\x1b[31m{}\n{}\n{}\x1b[0m'.format(
+                response.status_code,
+                response.headers,
+                pformat(response.content),
+            )
+        return response
+
+
+######################## timer utility ############################
+@contextmanager
+def timeblock(label): 
+    start = time.time() #time.process_time() available in python 3
+    try:
+        yield
+    finally:
+        end = time.time()
+        print ('{0} : {1:.10f} secs'.format(label, end - start))
 
 ################ pretty print POST (debugging)  ##################
 def pretty_print_POST(req):
@@ -62,119 +75,163 @@ def pretty_print_POST(req):
         req.body,
     ))
 
-################ refresh_creds ##################
-def refresh_creds():
+########## oauthFromSecret() ###################
+def oauthFromSecret(cli,sec):
+    if DEBUG: 
+        print 'in oauthFromSecret()'
+    #create oauth from client, secret
+    print 'creating oauth object from client and secret'
+    oauth = OAuth2(
+        client_id=cli,
+        client_secret=sec
+    )
+    auth_url, csrf_token = oauth.get_authorization_url(redir_uri)
+    print 'Cut/Paste this URI into the URL box in \
+        \na browser window and press enter:\n\n{0}\n'.format(auth_url)
+    print 'You should login and authorize use of your account by this app'
+    print 'You will then ultimately be redirected to a URL and a page \
+        that says "Connection Refused"'
+    auth_code = raw_input('Type in the code that appears after \
+        "code="\nin the URL box in your browser window, and press enter: ')
+    #csrf_returned = raw_input('Type in the csrf in the URI: ')
+    #assert csrf_returned == csrf_token
+    access_token, refresh_token = oauth.authenticate(auth_code)
+    writeTokens(access_token,refresh_token)
+    return oauth
 
-    '''
-    Utility to refresh OAuth2 access_token.
-    '''
-    global storage
-    print 'oauth2_cli_util:in refresh_creds'
-    print 'Token has expired... refreshing'
-    print redir_uri
+########## oauthFromTokens() ###################
+def oauthFromTokens(acc,ref,cli,sec):
+    #get oauth object from tokens, TODO: check if refresh works
+    oauth = None
+    try:
+	print 'creating oauth object from stored tokens'
+        oauth = OAuth2(
+            client_id=cli,
+            client_secret=sec,
+            access_token=acc,
+            refresh_token=ref
+        )
+    except Exception as e:  
+	print 'Exception in auth(...): {0}'.format(e)
+        oauth = None
 
-    creds = json.loads(CLI_session['credentials']) 
-    payload = {'grant_type':'refresh_token', 
-        'refresh_token':creds['refresh_token'],
-        'client_id':creds['client_id'], 
-        'client_secret':creds['client_secret'], 
-    }
-    headers={
-      'Content-Type':'application/x-www-form-urlencoded',
-    }
+    if oauth is None:
+	print 'oauth is None, calling create from secret'
+        return oauthFromSecret(cli,sec)
 
+    if acc != oauth._access_token or ref != oauth._refresh_token:
+	print 'oauthFromTokens: writing out tokens'
+        writeTokens(oauth._access_token,oauth._refresh_token)
+    return oauth
+
+########## auth() ###################
+def auth(cli,sec):
+    if DEBUG: 
+        print 'in auth()'
+    access_token,refresh_token = readTokens() 
+    oauth = None
+    if access_token is None:
+	oauth = oauthFromSecret(cli,sec)
+    else: 
+	oauth = oauthFromTokens(access_token,refresh_token,cli,sec)
+    return oauth
+
+#############################
+def setupClient():
+    #this assumes that initialize has been called
+    if clientID == 'YYY':
+        print 'setupStorage must be called before setupClient'
+        sys.exit(1)
+	
+    oauth = auth(clientID,secret)
+    if oauth is None:
+	print 'unable to create oauth2 object'
+        sys.exit(1)
     if DEBUG:
-        print '\nrefresh payload: {0}\n\ttoken_uri {1}\n\t{2}'.format(payload,creds['token_uri'],headers)
-    req = requests.Request('POST',creds['token_uri'],data=payload,headers=headers)
-    prepared = req.prepare()
-    pretty_print_POST(prepared)
-    s = requests.Session()
-    resp = s.send(prepared)
-    res = resp.json()
+        client = Client(oauth,LoggingNetwork())
+    else: 
+        client = Client(oauth)
+    if client is None:
+	print 'unable to create Box Client object'
+        sys.exit(1)
 
+    #use this to access box
+    return client
+
+################ initialize ##################
+def initialize(client,sec,redir,token):
+    global redir_uri,clientID,secret,token_uri,boxClient
     if DEBUG:
-        print 'result: {0}'.format(res)
-    if 'error' in res:
-        print 'refresh error: {0}'.format(res['error'])
-        ''' any error that occurs in the refresh process will result in restarting the protocol.
-            Protocol restart (oauth2setup) requires the user/customer to reauthorize our
-            use of their account '''
-        return False
-
-    ''' 
-    Create a new creds object and save it off on disk for fast access.
-    Note that the object saved on disk is clear text and contains the secret.
-    '''
-    refr = creds['refresh_token']
-    if res['refresh_token'] is not None:
-        print 'got a valid refresh token'
-        refr = res['refresh_token'] #get the new refresh token if any
-    if DEBUG:
-        print 'refresh token: {0}'.format(refr)
-
-    credentials = oclient.OAuth2Credentials(access_token=res['access_token'],
-        client_id=creds['client_id'],
-        client_secret=creds['client_secret'],
-        refresh_token=refr,
-        token_expiry=datetime.now()+timedelta(seconds=res['expires_in']),
-        token_uri=creds['token_uri'],user_agent=creds['user_agent'])
-    fname = None
-    if storage is None:
-        fname = '{0}_{1}'.format(app_name,service)
-        storage = ostorage(fname)
-    storage.put(credentials)
-    CLI_session['credentials'] = oclient.OAuth2Credentials.to_json(credentials)
-    if DEBUG:
-        print 'Stored creds at={0} and in file if not None {1}'.format(res['access_token'],fname)
-    return True
-
-################ setupStorage ##################
-def setupStorage(app,client,redir):
-    global storage,redir_uri,app_name,service
-    print 'oauth2_cli_util:in setupStorage {0},{1},{2}'.format(app,client,redir)
-    app_name = app
-    service = client
-    fname = '{0}_{1}'.format(app_name,service)
-    print 'setting up redir: {0}'.format(redir)
+        print 'oauth2_cli_util:in setupStorage {0},{1}'.format(client,redir)
+    clientID = client
+    secret = sec
     redir_uri=redir
-    storage = ostorage(fname)
+    token_uri=token
+    #boxClient = setupClient()
+    refresh_creds()
+    boxClient =  setupClient();
 
-################ getTokens ##################
-def getTokens():
+################ readTokens ##################
+def readTokens():
+    if not os.path.isfile('tokens'):
+        return None,None
+    with open('tokens', 'rb') as f:
+        acc = f.readline().rstrip('\n')
+        ref = f.readline().rstrip('\n')
+    return acc,ref
+
+################ writeTokens ##################
+def writeTokens(acc,ref):
+    with open('tokens', 'wb') as f:
+        f.write(acc+'\n')
+        f.write(ref+'\n')
     
-    '''
-    This function returns the access token or None
-    '''
-    print 'oauth2_cli_util:in getTotkens'
-    if 'credentials' not in CLI_session:
-        #check first to see if we have valid credentials 
-	#stored away from a previous run
-        if storage is None:
-            return None,None
-        if not initialize_storage():
-            return None,None
-    creds = json.loads(CLI_session['credentials']) #json
-    return creds['access_token'],creds['refresh_token']
+#############################
+def get_folder_using_boxclient(fid):    
+    try:
+        folder = boxClient.folder( folder_id=fid, ).get()
+    except: #try again
+        folder = boxClient.folder( folder_id=fid, ).get()
+    return folder
+
+#############################
+def get_file_using_boxclient(fid):    
+    global boxClient
+    f = None
+    try:
+        f = boxClient.file( file_id=fid, ).get()
+    except boxsdk.exception.BoxOAuthException as boxe: 
+        print boxe.__dict__
+        if boxe._status == 400 or boxe._status == 401:
+            refresh_creds()
+	    boxClient =  setupClient();
+        try:
+            f = boxClient.file( file_id=fid, ).get()
+        except Exception as e : #try again
+ 	    print e
+ 	    print type(e)
+ 	    print e.__dict__
+	    print f
+	    sys.exit(1)
     
+    return f
+
+
 ################ get_thumbnail ##################
 def get_thumbnail(fileID):
     
     '''
-    This function is invoked to access the API at api_url
-    It uses stored credentials, or creates them if not stored
+    This function is invoked to access the API at api_url using requests
+    It uses stored credentials, or creates them if not stored.
+    It does NOT use the Box Client
     get thumbnail:
     curl --header "Authorization: Bearer XXX" https://api.box.com/2.0/files/94082555506/thumbnail.png\?min_height=256\&min_width=256 -o test.png
     '''
-    print 'oauth2_cli_util:get_thumbnail'
-    if 'credentials' not in CLI_session:
-        #check first to see if we have valid credentials 
-	#stored away from a previous run
-        if not initialize_storage():
-            output = {'name':'oauth2_required'}
-            return output
-    creds = json.loads(CLI_session['credentials']) #json
+    if DEBUG:
+	print 'oauth2_cli_util:get_thumbnail'
+    access_token,refresh_token = readTokens() 
     
-    header = {'Authorization': 'Bearer {0}'.format(creds['access_token'])}
+    header = {'Authorization': 'Bearer {0}'.format(access_token)}
     api_url = 'https://api.box.com/2.0/files/{0}/thumbnail.png'.format(fileID)
     payload = {'min_height':'256','min_width':'256'}
     r = None
@@ -190,14 +247,14 @@ def get_thumbnail(fileID):
         if r.status_code == 401:  #unauthorized - check if refresh is needed, else regenerate from code
 
             #todo: this returns false up on failure, handle it better
-            success = refresh_creds()
+            #success = refresh_creds()
+            success = None
 	    if not success:
                 print 'refresh failed'
                 sys.exit(1)
 
             #do it again
-            creds = json.loads(CLI_session['credentials']) #json
-            header = {'Authorization': 'Bearer {0}'.format(creds['access_token'])}
+            header = {'Authorization': 'Bearer {0}'.format(access_token)}
             r = None
             try:
                 r = requests.get(api_url, headers=header)
@@ -221,114 +278,47 @@ def get_thumbnail(fileID):
 
     return output
 
+################ refresh_creds ##################
+def refresh_creds():
 
-################ query ##################
-def query(api_url):
-    
     '''
-    This function is invoked to access the API at api_url
-    It uses stored credentials, or creates them if not stored
+    Utility to refresh OAuth2 access_token.
     '''
-    print 'oauth2_cli_util:in query'
-    if 'credentials' not in CLI_session:
-        #check first to see if we have valid credentials 
-	#stored away from a previous run
-        if not initialize_storage():
-            output = {'name':'oauth2_required'}
-            return output
-    creds = json.loads(CLI_session['credentials']) #json
-    
-    header = {'Authorization': 'Bearer {0}'.format(creds['access_token'])}
-    '''get thumbnail:
-    curl https://api.box.com/2.0/files/FILE_ID/thumbnail.png?min_height=256&min_width=256 \
-    -H "Authorization: Bearer ACCESS_TOKEN"
-    '''
-    
+    if DEBUG: 
+	print 'oauth2_cli_util:in refresh_creds'
+    print 'Token has expired... refreshing'
+    print redir_uri
+    access_token,refresh_token = readTokens() 
+
     payload = {'grant_type':'refresh_token', 
-        'refresh_token':creds['refresh_token'],
-        'client_id':creds['client_id'], 
-        'client_secret':creds['client_secret'], 
-        #'redirect_uri':redir_uri,
+        'refresh_token':refresh_token,
+        'client_id':clientID, 
+        'client_secret':secret, 
     }
-    r = None
-    try:
-        r = requests.get(api_url, headers=header)
-    except requests.exceptions.RequestException as e:  
-        print 'API Access failed: {0}'.format(e)
-        output = {'name':'api_access_failed'}
-        return output
-
-    if r is not None:
-	#This is where we insert the code for storing the data that comes back from the request
-        if r.status_code == 401:  #unauthorized - check if refresh is needed, else regenerate from code
-
-            #todo: this returns false up on failure, handle it better
-            success = refresh_creds()
-	    if not success:
-                print 'refresh failed'
-                sys.exit(1)
-
-            #do it again
-            creds = json.loads(CLI_session['credentials']) #json
-            header = {'Authorization': 'Bearer {0}'.format(creds['access_token'])}
-            r = None
-            try:
-                r = requests.get(api_url, headers=header)
-            except requests.exceptions.RequestException as e:  
-                print 'API Access post refresh failed: {0}'.format(e)
-                output = {'name':'api_access_post_refresh_failed'}
-                return output
-
-        if r is not None:
-	    output = r.json()
-        else: 
-            output = {'name':'api_access_failed2'}
-    else: 
-        output = {'name':'api_access_failed3'}
-
-    return output
+    headers={
+      'Content-Type':'application/x-www-form-urlencoded'
+    }
 
 
-def setupOAuth2(auth,token,redir,client,secret,app,obs_scope=''):
-    global app_name,service,storage,redir_uri
-    print 'oauth2_cli_util:setupOAuth2'
-    try:
-        flow = oclient.OAuth2WebServerFlow(
-            client_id=client,
-            client_secret=secret,
-            scope=obs_scope,
-            redirect_uri=redir,
-            auth_uri=auth,
-            token_uri=token,
-            include_granted_scopes=True)
-    except: 
-        print traceback.format_exc()
-        return None
+    if DEBUG:
+        print '\nrefresh payload: {0}\n\theader {1}'.format(payload,headers)
+    #res = requests.post(token_uri,headers=headers,data=payload)
+    #print res
 
-    auth_uri = flow.step1_get_authorize_url()
-    redir_uri = redir
+    req = requests.Request('POST',token_uri,data=payload,headers=headers)
+    prepared = req.prepare()
+    pretty_print_POST(prepared)
+    s = requests.Session()
+    resp = s.send(prepared)
+    res = resp.json()
+    if DEBUG:
+        print 'result: {0}'.format(res)
+    if 'error' in res:
+        print 'refresh error: {0}'.format(res['error'])
+        ''' any error that occurs in the refresh process will result in restarting the protocol.
+            Protocol restart (oauth2setup) requires the user/customer to reauthorize our
+            use of their account '''
+        return False
 
-    print 'Cut/Paste this URI into the URL box in \
-        \na browser window and press enter:\n\n{0}\n'.format(auth_uri)
-    print 'You should login and authorize use of your account by this app'
-    print 'You will then ultimately be redirected to a URL and a page \
-        that says "Connection Refused"'
-    auth_code = raw_input('Type in the code that appears after \
-        "code="\nin the URL box in your browser window, and press enter: ')
-
-    try:
-        credentials = flow.step2_exchange(auth_code)
-    except oclient.FlowExchangeError, e:
-        print 'Authentication failed: {0}'.format(e)
-    
-    app_name = app
-    service = client
-    if storage is None:
-        fname = '{0}_{1}'.format(app_name,service)
-        storage = ostorage(fname)
-    storage.put(credentials)
-    json_creds = oclient.OAuth2Credentials.to_json(credentials)
-    CLI_session['credentials'] = json_creds
-    return flow
-
-
+    writeTokens(res['access_token'],res['refresh_token'])
+    return True
